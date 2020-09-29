28a4e9f73814eca354d66ed0a0455ddb8d5b569b
AbugaAroni <abugaaroni@gmail.com>
2020-09-29 17:13:46 +0300

    add the database connection

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abuga:password@localhost/Imeal'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class ProdConfig(Config):
