e46890f558c1b5294a9e033f54f1bebd8cfb404f
J-Okoto <okotojr@gmail.com>
2020-09-29 21:50:52 +0300

    added authentication system

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model,UserMixin):
@@ -12,17 +17,17 @@ class User(db.Model,UserMixin):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure  = db.Column(db.String(255))
 
-        @property
-        def password(self):
-            raise AttributeError('You cannot read the password attribute')
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-        @password.setter
-        def password(self, password):
-            self.pass_secure = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
 
-        def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     
 
