b3b4a2a862ec4ab83da68f4ac450ed0464f4c516
kahenya-anita <anitakahenya1@gmail.com>
2020-09-29 08:16:20 +0300

    Changing folder structure

---
+++ app/main/views.py
@@ -0,0 +1,20 @@
+from flask import render_template, request, redirect,url_for
+from . import main
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
+
+@main.route('/orders/<int:orders_id>')
+@login_required
+def orders(movie_id):
+
+    '''
+    View orders page function that returns the order details page and its data
+    '''
+    return render_template('orders.html',id = orders_id)
