bfbc0e54d39b36ed465846047a8a8adda14cffbe
Ted Mwangi <tedletheorde@gmail.com>
2020-10-01 18:33:23 +0300

    changess

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     '''
     General configuration parent class
     '''
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abuga:password@localhost/imeal'
+    SECRET_KEY = ('1201ccc5804c3210fecc')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thanos:qwerty@localhost/imeal'
     #heroku database below
     #SQLALCHEMY_DATABASE_URI ='postgres+psycopg2://lfgiafilewplkg:652bbbf039b88462ef1ce2fc405a8a4a434e53c197f30b192ca0ff51e636234b@ec2-3-210-255-177.compute-1.amazonaws.com:5432/d8jd2q7ohb0q7k'
 
