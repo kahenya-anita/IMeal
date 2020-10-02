c41e42b326a929a1906870f38670105e0dc27184
AbugaAroni <abugaaroni@gmail.com>
2020-10-02 12:54:10 +0300

    switched to the production model

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abuga:password@localhost/imeal'
     #heroku database below
-    SQLALCHEMY_DATABASE_URI ='postgres://ylazfenhkwzwpv:a38e1a7e4a7cd82e42e531b8e90b22240cc1345462add3cf7762806399289dfb@ec2-52-20-160-44.compute-1.amazonaws.com:5432/dt204hkjoqd90'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     #  email configurations
@@ -24,13 +23,8 @@ class Config:
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
+    SQLALCHEMY_DATABASE_URI ='postgres://ihpwxbflvtcaaf:08c66d8aae57327ba380813479a623fc7bc871f6ed109c8fb4acfc3a38125ffa@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d74vbju4helc1q'
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
 
 
 class DevConfig(Config):
