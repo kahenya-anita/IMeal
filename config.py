0f6e813ca15d7026a99823bb3229ab7c52ccc657
kahenya-anita <anitakahenya1@gmail.com>
2020-10-02 10:11:34 +0300

    Added links

--- config.py
+++ config.py
@@ -5,13 +5,8 @@ class Config:
     '''
     General configuration parent class
     '''
-<<<<<<< HEAD
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://toshiba:2543@localhost/imeal'
-=======
-    SECRET_KEY = ('1201ccc5804c3210fecc')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thanos:qwerty@localhost/imeal'
->>>>>>> 0e2c201729fb816f9ba1e002455b4e421962f30f
     #heroku database below
     #SQLALCHEMY_DATABASE_URI ='postgres+psycopg2://lfgiafilewplkg:652bbbf039b88462ef1ce2fc405a8a4a434e53c197f30b192ca0ff51e636234b@ec2-3-210-255-177.compute-1.amazonaws.com:5432/d8jd2q7ohb0q7k'
 
